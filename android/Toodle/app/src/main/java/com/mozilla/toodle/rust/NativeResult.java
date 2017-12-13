8fa90f89067b27d4a48c86cf15810f25987c79c2
Grisha Kruglov <gkruglov@mozilla.com>
2017-12-13 17:48:07 -0500

    cleanup

--- android/Toodle/app/src/main/java/com/mozilla/toodle/rust/NativeResult.java
+++ android/Toodle/app/src/main/java/com/mozilla/toodle/rust/NativeResult.java
@@ -1,3 +1,8 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package com.mozilla.toodle.rust;
 
 import com.sun.jna.Pointer;
@@ -15,8 +20,8 @@ public class NativeResult extends Structure implements Closeable {
     public static class ByValue extends NativeItem implements Structure.ByValue {
     }
 
-    public Pointer obj;
-    public String error;
+    /* package-private */ Pointer obj;
+    /* package-private */ String error;
 
     @Override
     protected List<String> getFieldOrder() {
